5f064bd67c5a2a515f4c552725891d6bcf71718d
Festus1914 <oladiplentyfestus@gmail.com>
2023-10-14 17:06:26 +0000

    creating a unique table for user

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+--This actually creates a table with unique users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+	id INT NOT NULL  PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+       	name VARCHAR(255)	
+
+);
